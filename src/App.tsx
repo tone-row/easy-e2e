e10eba73e2b8d3adabfc0b94e3ad94ac11a825d0
Rob Gordon <rchristopher.gordon@gmail.com>
2021-06-22 16:25:25 -0400

    Test change for new deploy preview url

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <input value={value} onChange={e => setValue(e.target.value)} data-testid="user input"/>
+        <p>
+          Change something about the site to see if the deploy preview url
+          updates
+        </p>
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          data-testid="user input"
+        />
         <h1>{value}</h1>
       </header>
     </div>
